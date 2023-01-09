a1c8e43cbe9168eb927d83224b794799c0ded038
fossouo <fossouodonald@gmail.com>
2023-01-09 13:30:14 +0100

    Update hello_lambda.py

--- hello_lambda.py
+++ hello_lambda.py
@@ -1,5 +1,24 @@
+import json 
+
+def tmi_evaluator(salaire:int):
+    if(salaire < 10226):
+        tmi = 0
+    elif(salaire > 10226 and salaire < 26070):
+        tmi = 11
+    elif(salaire > 26070 and salaire < 74546):
+        tmi = 30
+    elif(salaire > 74546 and salaire < 160336):
+        tmi = 41
+    else:
+        tmi = 45
+    return tmi
+
 def handler(event, context):
     return { "message": "Hello, World!" }
 
 def post_handler(event, context):
-    return { "message": "I should have created something..." }
+    
+    myname = event["Name"]
+    salaire = int(event["Salaire"])
+    response = tmi_evaluator(salaire)
+    return { "message": myname+' a une tmi de : '+str(response) }
