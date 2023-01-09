d5f45464996d99f4351a10968986456df6b08dcd
fossouo <fossouodonald@gmail.com>
2023-01-09 13:50:24 +0100

    Update hello_lambda.py

--- hello_lambda.py
+++ hello_lambda.py
@@ -13,6 +13,19 @@ def tmi_evaluator(salaire:int):
         tmi = 45
     return tmi
 
+def nb_part_fiscales(nb_enfants:int,maries:bol,pacse:bol):
+    if(maries==True or pacse==True):
+        if(nb_enfants<3):
+            nb_part_fiscales = 2+nb_enfants*0.5
+        else:
+            nb_part_fiscales = 2+(nb_enfants-2)*1 + 1
+    else:
+        if(nb_enfants<3):
+            nb_part_fiscales = 2+nb_enfants*0.5
+        else:
+            nb_part_fiscales = 2+(nb_enfants-2)*1 + nb_enfants*0.5
+    return nb_part_fiscales
+
 def handler(event, context):
     return { "message": "Hello, World!" }
 
